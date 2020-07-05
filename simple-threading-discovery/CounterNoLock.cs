4f79c69f4c0ad65a5a2efc0043e7499a5cdb2d21
Luka Radovanovic <lukadlm97@gmail.com>
2020-07-06 00:34:51 +0200

    thread lock with interlocked

---
+++ simple-threading-discovery/CounterNoLock.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace simple_threading_discovery
+{
+    class CounterNoLock : CounterBase
+    {
+        private int _count;
+        public int Count { get; set; }
+        public override void Decrement()
+        {
+            Interlocked.Decrement(ref _count);
+        }
+
+        public override void Increment()
+        {
+            Interlocked.Increment(ref _count);
+        }
+    }
+}
--- simple-threading-discovery/Program.cs
+++ simple-threading-discovery/Program.cs
@@ -41,7 +41,47 @@ namespace simple_threading_discovery
             //RunThreads();
             // RunGrounds();
 
-            ThreadWithLocks();
+            ThreadWithoutLocks();
+        }
+
+        private static void ThreadWithoutLocks()
+        {
+            Console.WriteLine("Incorrect counter");
+
+            var c = new Counter();
+
+            var t1 = new Thread(() => TestCounter(c));
+            var t2 = new Thread(() => TestCounter(c));
+            var t3 = new Thread(() => TestCounter(c));
+
+            t1.Start();
+            t2.Start();
+            t3.Start();
+            t1.Join();
+            t2.Join();
+            t3.Join();
+
+            Console.WriteLine("Total count: {0}", c.Count);
+            Console.WriteLine("-------------------------------");
+
+            Console.WriteLine("Incorrect counter");
+
+            var c1 = new CounterNoLock();
+
+            t1 = new Thread(() => TestCounter(c1));
+            t2 = new Thread(() => TestCounter(c1));
+            t3 = new Thread(() => TestCounter(c1));
+
+            t1.Start();
+            t2.Start();
+            t3.Start();
+            t1.Join();
+            t2.Join();
+            t3.Join();
+
+            Console.WriteLine("Total count: {0}", c1.Count);
+            Console.WriteLine("-------------------------------");
+
         }
 
         private static void ThreadWithLocks()
